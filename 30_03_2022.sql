f47a3e212e99412f1d173f20e9dcc4f217fa8df7
victor willian <47677499+V1ctorW1ll1an@users.noreply.github.com>
2022-03-30 21:17:38 -0300

    Create 30_03_2022.sql

---
+++ 30_03_2022.sql
@@ -0,0 +1,74 @@
+SELECT * FROM mercadinho.funcionario;
+
+INSERT INTO mercadinho.funcionario (nome_func, rua, cidade) VALUES ('coyote', 'toon', 'hollyqood');
+INSERT INTO mercadinho.funcionario (nome_func, rua, cidade) VALUES ('rabbit', 'tunnel', 'carrotville');
+INSERT INTO mercadinho.funcionario (nome_func, rua, cidade) VALUES ('smith', 'revolver', 'death valley');
+INSERT INTO mercadinho.funcionario (nome_func, rua, cidade) VALUES ('willians', 'seaview', 'seatle');
+
+SELECT * FROM mercadinho.funcionario_pi;
+
+INSERT INTO mercadinho.funcionario_pi (nome_agencia, nome_func, salario) VALUES ('mesa', 'coyote', '1500');
+INSERT INTO mercadinho.funcionario_pi (nome_agencia, nome_func, salario) VALUES ('mesa', 'rabbit', '1300');
+INSERT INTO mercadinho.funcionario_pi (nome_agencia, nome_func, salario) VALUES ('redmond', 'willians', '5300');
+INSERT INTO mercadinho.funcionario_pi (nome_agencia, nome_func, salario) VALUES ('redmond', 'gates', '1500');
+INSERT INTO mercadinho.funcionario_pi VALUES (5, 'redmond', 'bill', '1600');
+
+CREATE VIEW f AS
+SELECT * FROM mercadinho.funcionario;
+
+CREATE VIEW fp AS
+SELECT * FROM mercadinho.funcionario_pi;
+
+
+select * from f
+	join
+	fp
+    on
+    f.nome_func = fp.nome_func;
+    
+select * from f
+	left join
+	fp
+	on
+	f.nome_func = fp.nome_func;
+    
+select * from f
+	right join
+	fp
+	on
+	f.nome_func = fp.nome_func;
+
+select * from mercadinho.funcionario as fun
+	join
+	mercadinho.funcionario_pi as func_pi
+    on
+    fun.nome_func = func_pi.nome_func;
+    
+select * from f
+	join
+	fp
+	on
+	f.nome_func = fp.nome_func;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
